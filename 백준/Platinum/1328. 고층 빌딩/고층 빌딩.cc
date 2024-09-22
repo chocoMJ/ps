b7f02e2d0b9b939fc99fca971426aa3ee9e6701a
chocoMJ <119664259+chocoMJ@users.noreply.github.com>
2024-09-22 20:31:19 +0900

    [Platinum V] Title: 고층 빌딩, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Platinum/1328. 고층 빌딩/고층 빌딩.cc
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <cstring>  // memset 사용
+
+using namespace std;
+
+long long dp[101][101];
+long long C[101][101];
+long long f[101];
+
+const long long MOD = 1000000007;
+
+static void set_C() {
+    C[0][0] = 1;
+    C[1][0] = 1;
+    C[1][1] = 1;
+
+    for (int i = 2; i <= 100; i++) {
+        for (int j = 0; j <= i; j++) {
+            if (j == 0 || j == i) {
+                C[i][j] = 1;
+            } else {
+                C[i][j] = (C[i - 1][j - 1] + C[i - 1][j]) % MOD;
+            }
+        }
+    }
+}
+
+void set_f() {
+    f[0] = 1;
+    f[1] = 1;
+
+    for (int i = 2; i <= 100; i++) {
+        f[i] = (i * f[i - 1]) % MOD;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    long long ans = 0;
+    long long N, L, R;
+
+    memset(dp, 0, sizeof(dp));  // dp 배열 초기화
+    set_C();
+    set_f();
+
+    cin >> N >> L >> R;
+
+    if (N <= 1) {
+        cout << 1 << endl;
+        return 0;
+    }
+
+    for (int i = 1; i < N; i++) {
+        for (int j = 1; j <= i; j++) {
+            if (j == 1) {
+                dp[i][j] = f[i - 1];
+            } else if (j == i) {
+                dp[i][j] = 1;
+            } else {
+                for (int pos = j; pos <= i; pos++) {
+                    dp[i][j] += (dp[pos - 1][j - 1] * f[i - pos] % MOD * C[i - 1][pos - 1] % MOD) % MOD;
+                    dp[i][j] %= MOD;
+                }
+            }
+        }
+    }
+
+    for (long long pos = L; pos <= N - R + 1; pos++) {
+        if (pos == 1) {
+            ans += (C[N - 1][pos - 1] * dp[N - pos][R - 1]) % MOD;
+        } else if (pos == N) {
+            ans += (C[N - 1][pos - 1] * dp[pos - 1][L - 1]) % MOD;
+        } else {
+            ans += (C[N - 1][pos - 1] * dp[pos - 1][L - 1] % MOD * dp[N - pos][R - 1] % MOD) % MOD;
+        }
+        ans %= MOD;  // 덧셈 후 % MOD 적용
+    }
+
+    cout << ans % MOD << endl;
+
+    return 0;
+}
