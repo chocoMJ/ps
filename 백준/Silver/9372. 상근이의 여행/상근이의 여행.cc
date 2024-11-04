1879ccdad8a32cb3a3e721c3f727b446278546ee
chocoMJ <119664259+chocoMJ@users.noreply.github.com>
2024-11-04 21:13:26 +0900

    [Silver IV] Title: 상근이의 여행, Time: 68 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	int N, M;
+
+	while (T--) {
+		cin >> N >> M;
+		while (M--) {
+			int a, b;
+			cin >> a >> b;
+		}
+		cout << N - 1 << endl;
+	}
+
+	return 0;
+}
